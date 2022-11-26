fe786e28bd23cf4e8d61a99d5bf65dde57e69ec3
Dmitry Konishchev <konishchev@gmail.com>
2022-11-26 12:23:01 +0300

    Implement process printing

--- my.c
+++ my.c
@@ -1,61 +1,92 @@
-#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+#define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
+//#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
 
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/sched.h>	/* current() */
-#include <linux/preempt.h>	/* in_task() */
-#include <linux/cred.h>		/* current_{e}{u,g}id() */
-#include <linux/uidgid.h>	/* {from,make}_kuid() */
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/preempt.h>
+#include <linux/cred.h>
+#include <linux/uidgid.h>
 
 MODULE_AUTHOR("Dmitry Konishchev");
 MODULE_DESCRIPTION("My Linux development playground");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
-static void show_ctx(void) {
-	/* Extract the task UID and EUID using helper methods provided */
-	unsigned int uid = from_kuid(&init_user_ns, current_uid());
-	unsigned int euid = from_kuid(&init_user_ns, current_euid());
+static void print_process_info(struct task_struct* process, bool print_threads) {
+	{
+		task_lock(process);
 
-	if (!in_task()) {
-		pr_alert("in interrupt context [Should NOT Happen here!]\n");
+		uid_t uid = from_kuid(&init_user_ns, process->cred->uid);
+
+		const char* command_prefix = "";
+		const char* command_suffix = "";
+		if (!process->mm) {
+			command_prefix = "[";
+			command_suffix = "]";
+		}
+
+		pr_info(
+			"pid:%5d uid:%4d: %s%s%s\n",
+			process->tgid, uid, command_prefix, process->comm, command_suffix
+		);
+
+		task_unlock(process);
+	}
+
+	if (!print_threads) {
 		return;
 	}
 
-	pr_info("in process context ::\n"
-		" PID         : %6d\n"
-		" TGID        : %6d\n"
-		" UID         : %6u\n"
-		" EUID        : %6u (%s root)\n"
-		" name        : %s\n"
-		" current (ptr to our process context's task_struct) :\n"
-		"               0x%pK (0x%px)\n"
-		" stack start : 0x%pK (0x%px)\n",
-		/* always better to use the helper methods provided */
-		task_pid_nr(current), task_tgid_nr(current),
-		/* ... rather than the 'usual' direct lookups:
-			* current->pid, current->tgid,
-			*/
-		uid, euid,
-		(euid == 0 ? "have" : "don't have"),
-		current->comm,
-		current, current,
-		current->stack, current->stack);
+	struct task_struct* thread;
+	for_each_thread(process, thread) {
+		if (thread == process) {
+			continue;
+		}
+
+		task_lock(thread);
+
+		if (strcmp(process->comm, thread->comm)) {
+			pr_info("* tid:%5d: %s\n", thread->pid, thread->comm);
+		} else {
+			pr_info("* tid:%5d\n", thread->pid);
+		}
+
+		task_unlock(thread);
+	}
 }
 
-static int __init my_init(void)
-{
-	pr_info("The module is inserted.\n");
-	pr_info("sizeof(struct task_struct)=%zd\n", sizeof(struct task_struct));
-	show_ctx();
+static void print_all_processes(void) {
+	pr_info("All system processes:\n");
+
+	rcu_read_lock();
+
+	// CPU0 idle thread, i.e., the pid 0 task, the (terribly named) swapper/$cpu
+	print_process_info(&init_task, true);
+
+	struct task_struct* process;
+	for_each_process(process) {
+		print_process_info(process, true);
+	}
+
+	rcu_read_unlock();
+}
+
+static int __init my_init(void) {
+	pr_info("The module is inserted by:\n");
+	if (in_task()) {
+		print_process_info(current, false);
+	}
+
+	print_all_processes();
 	return 0;
 }
 
-static void __exit my_exit(void)
-{
-	show_ctx();
-	pr_info("The module is removed.\n");
+static void __exit my_exit(void) {
+	pr_info("The module is removed by:\n");
+	if (in_task()) {
+		print_process_info(current, false);
+	}
 }
 
 module_init(my_init);
